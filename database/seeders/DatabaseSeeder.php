7bb32db9078d69565cc85d38646d4a9009b1723a
Irfandyz <irfandyzize@gmail.com>
2023-11-23 01:44:20 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             SettingSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -24,5 +25,8 @@ class UserSeeder extends Seeder
                 'password'=>bcrypt('admin2'),
             ],
         ];
+        foreach ($data as $value) {
+            User::create($value);
+        }
     }
 }
