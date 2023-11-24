07a9fcd1892916245da24f55fb37ec52f80ad367
Irfandyz <irfandyzize@gmail.com>
2023-11-24 15:09:33 +0700

    update guide

--- resources/views/user/guide.blade.php
+++ resources/views/user/guide.blade.php
@@ -1,17 +1,21 @@
 <p class="mt-3">
-    What is Lorem Ipsum?
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-    Aldus PageMaker including versions of Lorem Ipsum.
-
-    Why do we use it?
-    It is a long established fact that a reader will be distracted by the readable content of a page when
-    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-    letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-    desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-    search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
-    over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    <h3 class="fw-bold">
+        Anda akan diminta untuk melakukan percobaan yang disebut dengan Realtime Respon Analyzer. Adapun hal yang harus anda lakukan sebagai brikut:
+    </h3>
+    <br>
+    <ol>
+        <li class="h5 fw-bold">
+            Pada layar akan muncul dua titik dimana akan muncul warna pada salah satu titik.
+        </li>
+        <li class="h5 fw-bold">
+            Tugas anda menekan tombol sesuai warna yang muncul dengan menekan tombol Alt+Q dan Alt+W.
+        </li>
+        <li class="h5 fw-bold">
+            Jika anda melakukan kesalahan dalam menekan tombol maka akan muncul peringatan hingga tombol yang dipencet sesuai dengan warnanya.
+        </li>
+        <li class="h5 fw-bold">
+             Lakukan hingga percobaan selesai.
+        </li>
+    </ol>
+    
 </p>
